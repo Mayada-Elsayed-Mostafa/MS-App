c5387bb83e6a6323c3790800bedc187bc2e78cca
Mayada Elsayed <emayada44@gmail.com>
2023-11-11 00:00:48 +0200

    last edition

--- app/src/main/java/com/example/msapplication/HomeFragment.kt
+++ app/src/main/java/com/example/msapplication/HomeFragment.kt
@@ -1,14 +1,13 @@
 package com.example.msapplication
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 class HomeFragment : Fragment() {
 
     private lateinit var appointmentsAdapter: AppointmentsAdapter
