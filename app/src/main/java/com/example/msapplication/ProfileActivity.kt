e2a6fa453224f3cfae272dc38f8e12e849e04b6f
Mayada Elsayed <emayada44@gmail.com>
2023-11-21 00:53:10 +0200

    last edition

--- app/src/main/java/com/example/msapplication/ProfileActivity.kt
+++ app/src/main/java/com/example/msapplication/ProfileActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
