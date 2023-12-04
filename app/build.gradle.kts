80b0f2eedeb85b87a21e74571f4cc73ca24096a5
Mayada Elsayed <emayada44@gmail.com>
2023-12-04 17:55:42 +0200

    last edition

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation ("")
+    implementation ("com.intuit.sdp:sdp-android:1.1.0")
 
     implementation ("androidx.cardview:cardview:1.0.0")
 
