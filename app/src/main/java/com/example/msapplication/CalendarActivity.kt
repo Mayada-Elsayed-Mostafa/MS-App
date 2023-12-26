b716ec4e7d3473b08ed7c402c47854c8c3b04d0a
Mayada Elsayed <emayada44@gmail.com>
2023-12-26 02:21:35 +0200

    last edition

--- app/src/main/java/com/example/msapplication/CalendarActivity.kt
+++ app/src/main/java/com/example/msapplication/CalendarActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 import android.widget.CalendarView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.google.android.material.textfield.TextInputEditText
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -28,6 +29,21 @@ class CalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
+
+        // Reference the Toolbar from the layout
+        val toolbar = findViewById<Toolbar>(R.id.calendarToolbar!!)
+        toolbar.title = ""
+
+        // Set the Toolbar as the support action bar
+        setSupportActionBar(toolbar)
+
+        // Enable the back button (up button) in the action bar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Handle the back button click event
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
 
         eventNameEditText = findViewById(R.id.name)
         eventDetailsEditText = findViewById(R.id.details)
