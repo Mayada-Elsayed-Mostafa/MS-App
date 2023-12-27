a8685515df2350e5274a251e7ddc453eddd7c9b8
Mayada Elsayed <emayada44@gmail.com>
2023-12-27 03:29:36 +0200

    last edition

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,11 +53,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation ("com.intuit.sdp:sdp-android:1.1.0")
-    implementation ("com.intuit.ssp:ssp-android:1.1.0")
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
+    implementation("com.intuit.ssp:ssp-android:1.1.0")
 
 
-    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 
 
     implementation("androidx.core:core-splashscreen:1.0.1")
@@ -85,7 +85,8 @@ dependencies {
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth-ktx")
-    
+
+    implementation("com.balysv:material-ripple:1.0.2")
+
 
 }
-
