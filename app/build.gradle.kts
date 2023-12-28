73979eddea5960d642de28f00942a3e773b51147
Mayada Elsayed <emayada44@gmail.com>
2023-12-28 20:00:57 +0200

    last edition

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,9 +56,7 @@ dependencies {
     implementation("com.intuit.sdp:sdp-android:1.1.0")
     implementation("com.intuit.ssp:ssp-android:1.1.0")
 
-
     implementation("androidx.cardview:cardview:1.0.0")
-
 
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("de.hdodenhof:circleimageview:3.1.0")
