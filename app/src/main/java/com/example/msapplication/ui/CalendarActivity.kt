406b4be5ccc2a40ee32983a2cd3a4f4371f77605
Mayada Elsayed <emayada44@gmail.com>
2024-01-04 01:46:08 +0200

    last edition

--- app/src/main/java/com/example/msapplication/ui/CalendarActivity.kt
+++ app/src/main/java/com/example/msapplication/ui/CalendarActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 import android.widget.CalendarView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.msapplication.EventContract
 import com.example.msapplication.EventDatabaseHelper
 import com.example.msapplication.OnEventSavedListener
@@ -35,6 +36,22 @@ class CalendarActivity : AppCompatActivity(), OnEventSavedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
+
+
+        // Reference the Toolbar from the layout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.title = ""
+
+        // Set the Toolbar as the support action bar
+        setSupportActionBar(toolbar)
+
+        // Enable the back button (up button) in the action bar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Handle the back button click event
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
 
         eventNameEditText = findViewById(R.id.name)
         eventDetailsEditText = findViewById(R.id.details)
