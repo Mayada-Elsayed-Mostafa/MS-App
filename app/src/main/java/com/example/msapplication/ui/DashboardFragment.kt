ccc2b7cc3f008d7bf57cf6173eb59a049ccb8e49
Mayada Elsayed <emayada44@gmail.com>
2024-02-13 20:02:41 +0200

    last edition

--- app/src/main/java/com/example/msapplication/ui/DashboardFragment.kt
+++ app/src/main/java/com/example/msapplication/ui/DashboardFragment.kt
@@ -2,11 +2,11 @@ package com.example.msapplication.ui
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
 import com.example.msapplication.R
 
 class DashboardFragment : Fragment() {
